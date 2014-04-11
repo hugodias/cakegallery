a693e79cccff7e2354302a4b1015e627de35abe9
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2014-04-11 16:14:10 -0300

    behavior afterfind scope

--- Model/Behavior/GalleryBehavior.php
+++ Model/Behavior/GalleryBehavior.php
@@ -32,4 +32,14 @@ class GalleryBehavior extends ModelBehavior {
 			)
 		));
 	}
+
+	/**
+	 * Append gallery to model
+	 * @param Model $Model
+	 * @param $results
+	 * @param bool $primary
+	 */
+	public function afterFind(Model $Model, $results, $primary = false){
+		debug($results);
+	}
 }
