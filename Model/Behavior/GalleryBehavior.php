e1bff6c3eb054787096691877f50eaca360c3e19
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2014-04-11 16:11:03 -0300

    added behavior

---
+++ Model/Behavior/GalleryBehavior.php
@@ -0,0 +1,35 @@
+<?php
+App::uses('ModelBehavior', 'Model');
+App::uses('Gallery.Folder', 'Model');
+App::uses('Gallery.Record', 'Model');
+
+class GalleryBehavior extends ModelBehavior {
+
+	public function setup(Model $Model, $settings = array()) {
+		if (!isset($this->settings[$Model->alias])) {
+			$this->settings[$Model->alias] = array(
+				'option1_key' => 'option1_default_value',
+				'option2_key' => 'option2_default_value',
+				'option3_key' => 'option3_default_value',
+			);
+		}
+		$this->settings[$Model->alias] = array_merge(
+			$this->settings[$Model->alias], (array)$settings);
+	}
+
+
+	/**
+	 * Get model gallery
+	 * @param Model $Model
+	 * @return mixed
+	 */
+	public function getGallery(Model $Model){
+		$Folder = new Folder();
+		return $Folder->find('first', array(
+			'conditions' => array(
+				'model' => $Model->alias,
+				'model_id' => $Model->id
+			)
+		));
+	}
+}
