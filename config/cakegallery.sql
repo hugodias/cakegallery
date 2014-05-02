81dd2eab479d89225889de1c4b24a859c29d85ab
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2014-05-02 13:02:42 -0300

    Change database structure

--- config/cakegallery.sql
+++ config/cakegallery.sql
@@ -1,30 +1,29 @@
-CREATE TABLE `gallery_galleries` (
+CREATE TABLE `gallery_pictures` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `path` varchar(255) NOT NULL,
+  `size` bigint(20) NOT NULL,
+  `album_id` int(11) NOT NULL,
+  `main_id` int(11) DEFAULT NULL,
+  `style` varchar(255) NOT NULL DEFAULT 'full',
+  `cover` enum('Y','N') NOT NULL DEFAULT 'N',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `folder_id` (`album_id`),
+  KEY `main_id` (`main_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE `gallery_albums` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `default_name` varchar(255) NOT NULL,
   `path` varchar(255) NOT NULL,
   `model` varchar(255) DEFAULT NULL,
   `model_id` int(11) DEFAULT NULL,
-  `width` int(11) DEFAULT NULL,
-  `height` int(11) DEFAULT NULL,
-  `th` enum('Y','N') NOT NULL DEFAULT 'Y',
-  `th_width` int(11) NOT NULL,
-  `th_height` int(11) NOT NULL,
-  `action` varchar(255) NOT NULL DEFAULT 'proportional_resize',
+  `tags` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
   `created` datetime NOT NULL,
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-CREATE TABLE `gallery_pictures` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `path` varchar(255) NOT NULL,
-  `thumbnail_path` varchar(255) NOT NULL,
-  `size` bigint(20) NOT NULL,
-  `album_id` int(11) NOT NULL,
-  `created` datetime NOT NULL,
-  `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `album_id` (`album_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
