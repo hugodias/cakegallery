74eb6b8a025902d37795d580b7414fc235bccedd
HugoDias <hugo.victor.dias.teodoro@gmail.com>
2014-05-05 06:28:47 -0300

    spaces

--- Controller/GalleryAppController.php
+++ Controller/GalleryAppController.php
@@ -4,7 +4,7 @@ App::uses('Controller', 'Controller');
 class GalleryAppController extends AppController {
 
 	public $helpers = array('Gallery.Gallery', 'Form' => array('className' => 'Gallery.CakePHPFTPForm'));
-	
+
 	public $components = array('Gallery.Util');
 
 	public function beforeFilter() {
