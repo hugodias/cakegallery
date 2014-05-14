da358f55be87ca8dc0e72d01c279c5f7415832b4
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2014-05-14 12:39:13 -0300

    added travis

---
+++ Test/Case/AllGalleryTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * All Gallery plugin tests
+ */
+class AllGalleryTest extends CakeTestCase {
+
+/**
+ * Suite define the tests for this plugin
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new CakeTestSuite('All Gallery test');
+
+		$path = CakePlugin::path('Gallery') . 'Test' . DS . 'Case' . DS;
+		$suite->addTestDirectoryRecursive($path);
+
+		return $suite;
+	}
+
+}
