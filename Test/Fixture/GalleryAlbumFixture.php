f9005e5e5bca3ef49af8210cc69882a781a4c532
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2014-05-14 13:42:48 -0300

    Changing file name to match table name

---
+++ Test/Fixture/GalleryAlbumFixture.php
@@ -0,0 +1,58 @@
+<?php
+
+class GalleryAlbumFixture extends CakeTestFixture {
+	public $fields = array(
+		'id' => array('type' => 'integer', 'key' => 'primary'),
+		'title' => array('type' => 'string', 'length' => 255, 'null' => false),
+		'default_name' => array('type' => 'string', 'length' => 255, 'null' => false),
+		'path' => array('type' => 'string', 'length' => 255, 'null' => false),
+		'model' => array('type' => 'string', 'length' => 255, 'null' => true),
+		'model_id' => array('type' => 'integer', 'length' => 11, 'null' => true),
+		'tags' => array('type' => 'string', 'length' => 255, 'null' => true),
+		'status' => array('type' => 'string', 'length' => 255, 'null' => false),
+		'created' => 'datetime',
+		'updated' => 'datetime'
+	);
+
+	public function init() {
+		$this->records = array(
+			array(
+				'id' => 1,
+				'title' => 'First Album',
+				'default_name' => 'First Article Body',
+				'path' => '/1/',
+				'model' => null,
+				'model_id' => null,
+				'tags' => null,
+				'status' => 'published',
+				'created' => date('Y-m-d H:i:s'),
+				'updated' => date('Y-m-d H:i:s'),
+			),
+			array(
+				'id' => 2,
+				'title' => 'Second Album',
+				'default_name' => 'First Article Body',
+				'path' => '/1/',
+				'model' => null,
+				'model_id' => null,
+				'tags' => null,
+				'status' => 'draft',
+				'created' => date('Y-m-d H:i:s'),
+				'updated' => date('Y-m-d H:i:s'),
+			),
+			array(
+				'id' => 3,
+				'title' => 'Third Album',
+				'default_name' => 'First Article Body',
+				'path' => '/1/',
+				'model' => null,
+				'model_id' => null,
+				'tags' => null,
+				'status' => 'published',
+				'created' => date('Y-m-d H:i:s'),
+				'updated' => date('Y-m-d H:i:s'),
+			)
+		);
+		parent::init();
+	}
+}
