6e189d0aee8990e8c63e259701f60fbbf0fbe706
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2014-05-14 13:42:22 -0300

    Added extra test cases for album model

--- Test/Case/Model/AlbumTest.php
+++ Test/Case/Model/AlbumTest.php
@@ -2,22 +2,45 @@
 App::uses('Album', 'Gallery.Model');
 
 class AlbumTest extends CakeTestCase {
-    public $fixtures = array('plugin.gallery.album');
-    public $Album;
+	public $fixtures = array('plugin.gallery.gallery_album');
+	public $Album;
 
-    public function setUp() {
-        parent::setUp();
-        $this->Album = ClassRegistry::init('Gallery.Album');
-    }
+	public function setUp() {
+		parent::setUp();
+		$this->Album = ClassRegistry::init('Gallery.Album');
+	}
 
-    public function testPublished() {
-        $result = $this->Album->published(array('id','title'));
-        $expected = array(
-            array('Album' => array('id' => 1, 'title' => 'First Album')),
-            array('Album' => array('id' => 2, 'title' => 'Second album')),
-            array('Album' => array('id' => 3, 'title' => 'Third album'))
-        );
+	public function testPublished() {
+		$result = $this->Album->published(array('id', 'status'));
+		$expected = array(
+			array('Album' => array('id' => 3, 'status' => 'published'), 'Picture' => array()),
+			array('Album' => array('id' => 1, 'status' => 'published'), 'Picture' => array())
+		);
 
-        $this->assertEquals($expected, $result);
-    }
+		$this->assertEquals($expected, $result);
+	}
+
+	public function testDraft() {
+		$result = $this->Album->draft(array('id', 'status'));
+		$expected = array(
+			array('Album' => array('id' => 2, 'status' => 'draft'), 'Picture' => array()),
+		);
+
+		$this->assertEquals($expected, $result);
+	}
+
+	public function testCreateStandAloneAlbum() {
+		$result = $this->Album->createAlbumAndRedirect();
+
+		$this->assertTrue(is_array($result));
+	}
+
+	public function testCreateAttachedAlbum() {
+		$result = $this->Album->createAlbumAndRedirect('product', 1);
+
+		$this->assertTrue($result['Album']['model'] == "product");
+
+		$this->assertTrue($result['Album']['model_id'] == 1);
+	}
+
 }
