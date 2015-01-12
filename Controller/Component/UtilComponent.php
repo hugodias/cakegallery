a53b43c041f59452f0ff55cc63d9785c0724bbad
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2015-01-12 10:47:45 -0200

    Changing token generator function. Reason: compatibility with some php versions

--- Controller/Component/UtilComponent.php
+++ Controller/Component/UtilComponent.php
@@ -1,22 +1,11 @@
 <?php
+
 class UtilComponent extends Component
 {
 
-    public function crypto_rand_secure($min, $max)
+    public function crypto_rand_secure()
     {
-        $range = $max - $min;
-        if ($range < 0) {
-            return $min;
-        } // not so random...
-        $log = log($range, 2);
-        $bytes = (int)($log / 8) + 1; // length in bytes
-        $bits = (int)$log + 1; // length in bits
-        $filter = (int)(1 << $bits) - 1; // set all lower bits to 1
-        do {
-            $rnd = hexdec(bin2hex(openssl_random_pseudo_bytes($bytes)));
-            $rnd = $rnd & $filter; // discard irrelevant bits
-        } while ($rnd >= $range);
-        return $min + $rnd;
+        return md5(uniqid(mt_rand(), true));
     }
 
     public function getToken($length = 32)
@@ -25,7 +14,7 @@ class UtilComponent extends Component
         $codeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         $codeAlphabet .= "abcdefghijklmnopqrstuvwxyz";
         $codeAlphabet .= "0123456789";
-        for ($i = 0 ; $i < $length ; $i++) {
+        for ($i = 0; $i < $length; $i++) {
             $token .= $codeAlphabet[$this->crypto_rand_secure(0, strlen($codeAlphabet))];
         }
         return $token;
