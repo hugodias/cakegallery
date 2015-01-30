d287f748d5be8fbf0ea003a9c412853a719b0b42
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2015-01-30 11:42:03 -0200

    Changed helper name and added the docs

--- View/Helper/GalleryHelper.php
+++ View/Helper/GalleryHelper.php
@@ -4,7 +4,25 @@ App::uses('FormHelper', 'View/Helper');
 
 class GalleryHelper extends AppHelper
 {
-    public function button($model = null, $model_id = null, $html_options = array())
+    /**
+     * Gallery link helper
+     *
+     * This helper will create an link to a new or existing gallery
+     *
+     * If no arguments are passed this will create a new gallery everytime the link are clicked.
+     *
+     * When model & model_id arguments are passed, this will redirect to this specific gallery (it will also create a
+     * new one if didn't exists)
+     *
+     * Example:
+     * <?php echo $this->Gallery->button('product', 1) ?>
+     *
+     * @param null $model
+     * @param null $model_id
+     * @param array $html_options
+     * @return string
+     */
+    public function link($model = null, $model_id = null, $html_options = array())
     {
         return $this->_View->Html->link(
             'Upload pictures',
@@ -14,22 +32,6 @@ class GalleryHelper extends AppHelper
                 'plugin' => 'gallery',
                 $model,
                 $model_id
-            ),
-            $html_options
-        );
-    }
-
-    public function new_gallery_button($html_options = array())
-    {
-        # Icons on link
-        $html_options['escape'] = false;
-
-        return $this->_View->Html->link(
-            '<i class="fa fa-plus"></i> New Album',
-            array(
-                'controller' => 'albums',
-                'action' => 'upload',
-                'plugin' => 'gallery'
             ),
             $html_options
         );
