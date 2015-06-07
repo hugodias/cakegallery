78d3c78cee723cf5536dab12268063840989ecfc
Hugo Dias <hugo.victor.dias.teodoro@gmail.com>
2015-06-07 10:25:48 -0300

    added caption field in album table at sql file

--- Config/cakegallery.sql
+++ Config/cakegallery.sql
@@ -5,6 +5,7 @@ CREATE TABLE `gallery_pictures` (
   `size` bigint(20) NOT NULL,
   `album_id` int(11) NOT NULL,
   `main_id` int(11) DEFAULT NULL,
+  `caption` varchar(255) DEFAULT NULL,
   `style` varchar(255) NOT NULL DEFAULT 'full',
   `order` int(11) NOT NULL DEFAULT 9999999,
   `created` datetime NOT NULL,
